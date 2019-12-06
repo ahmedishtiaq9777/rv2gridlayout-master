e2700538d9ac7a824bf3addff6860a6f1cc6dbf3
ahmedishtiaq9777 <50663545+ahmedishtiaq9777@users.noreply.github.com>
2019-12-06 12:20:21 +0500

    Profile fragment completed

--- app/src/main/java/com/demotxt/myapp/recyclerview/ProfileFragment.java
+++ app/src/main/java/com/demotxt/myapp/recyclerview/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.demotxt.myapp.recyclerview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,7 +28,88 @@ public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+
+
+        View view = inflater.inflate(R.layout.profilefragment, container, false);
+
+        sharedPref = new SharedPref(getActivity());
+
+        lyt_root = view.findViewById(R.id.lyt_root);
+        if (Config.ENABLE_RTL_MODE) {
+            lyt_root.setRotationY(180);
+        }
+
+        txt_user_name = view.findViewById(R.id.txt_user_name);
+        txt_user_email = view.findViewById(R.id.txt_user_email);
+        txt_user_phone = view.findViewById(R.id.txt_user_phone);
+        txt_user_address = view.findViewById(R.id.txt_user_address);
+
+        btn_edit_user = view.findViewById(R.id.btn_edit_user);
+        btn_edit_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Intent intent = new Intent(getActivity(), ActivitySettings.class);
+                //startActivity(intent);
+            }
+        });
+
+        btn_order_history = view.findViewById(R.id.btn_order_history);
+        btn_order_history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // Intent intent = new Intent(getActivity(), ActivityHistory.class);
+             //   startActivity(intent);
+            }
+        });
+
+        btn_rate = view.findViewById(R.id.btn_rate);
+        btn_rate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               /* final String appName = getActivity().getPackageName();
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appName)));
+                } catch (android.content.ActivityNotFoundException anfe) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + appName)));
+
+                *?
+                */
+            }
+        });
+
+        btn_share = view.findViewById(R.id.btn_share);
+        btn_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               /* String share_text = Html.fromHtml(getResources().getString(R.string.share_app)).toString();
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, share_text + "\n\n" + "https://play.google.com/store/apps/details?id=" + getActivity().getPackageName());
+                intent.setType("text/plain");
+                startActivity(intent);
+                */
+
+            }
+        });
+
+        btn_privacy = view.findViewById(R.id.btn_privacy);
+        btn_privacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.privacy_policy_url))));
+            }
+        });
+
+        return view;
+
+
+
+
+
+
+
+
     }
 }
 
